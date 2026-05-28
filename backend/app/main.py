f3bc6412f1b257108f3a65f0d3bc0d5d89754397
sihomaramipc20223-tech <sihomaramipc2022.3@gmail.com>
2026-05-28 15:58:49 -0500

    Correcion gitnore

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import persons
+from routers import persons
 
 app = FastAPI(
     title="NTT Data - API de Personas",
